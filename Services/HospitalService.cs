a0c45f6a8f94e6bd89dede651b5c4c3d8fb60e57
isa2206 <hidalgoavilaisabel@gmail.com>
2025-10-27 04:55:25 -0400

    Feat: GetAllHospitals

--- Services/HospitalService.cs
+++ Services/HospitalService.cs
@@ -33,5 +33,34 @@ namespace Security.Services
         {
             return await _repo.GetOne(id);
         }
+
+        public async Task<Hospital?> UpdateHospital(UpdateHospitalDto dto)
+        {
+            var hospital = await _repo.GetOne(dto.Id);
+            if (hospital == null) return null;
+
+
+            if (dto.Name != null) hospital.Name = dto.Name;
+            if (dto.Address != null) hospital.Address = dto.Address;
+            if (dto.Type.HasValue) hospital.Type = dto.Type.Value;
+
+
+            await _repo.Update(hospital);
+            return hospital;
+        }
+
+        public async Task<bool> DeleteHospital(Guid id)
+        {
+            var hospital = await _repo.GetOne(id);
+            if (hospital == null) return false;
+
+            await _repo.Delete(hospital);
+            return true;
+        }
+
+        public async Task<IEnumerable<Hospital>> GetAllByTypes(IEnumerable<int> types)
+        {
+            return await _repo.GetAllByTypes(types);
+        }
     }
 }
