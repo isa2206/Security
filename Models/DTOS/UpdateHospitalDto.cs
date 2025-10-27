f2938d156ee62781a100ad6d34cf10d06a356fb0
isa2206 <hidalgoavilaisabel@gmail.com>
2025-10-27 04:48:10 -0400

    Feat: Implementation of UpdateHospitalDto

---
+++ Models/DTOS/UpdateHospitalDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Security.Models.DTOS
+{
+    public record UpdateHospitalDto
+    {
+        public Guid Id { get; init; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int? Type { get; set; }
+    }
+}
+
+
+
